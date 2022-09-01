b91f8b0328ab0d33590baccef316e697591dc5f4
lujolfs <lucas.j.frank@gmail.com>
2022-09-01 13:46:42 -0300

    Implementação da renderização de quizzes. Falta substituir o loop de for por uma alternativa mais eficiente.

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+//
+const quizzesBox = document.querySelector('.api-quizz');
+let quizzesApi = []; // Cria array vazio para preenchê-lo com as informações recebidas da API.
+renderQuizz(); // Chama a função render Quizz.
+
+
+function renderQuizz() {
+    let quizzesInfo = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+    quizzesInfo.then(fillArray);
+}
+
+function fillArray(quizzes) {
+    quizzesApi = quizzes.data;
+    printQuizz(quizzesApi);
+}
+
+function printQuizz(quizzesApi) {
+    quizzesBox.innerHTML = '';
+    console.log(quizzesApi);
+
+    for(let i = 0; i < 6; i++) { // Esse for é substituível por um map, mas não consegui aprender como
+    quizzesBox.innerHTML = quizzesBox.innerHTML + `
+    <div class="quizz box">
+        <div class="gradient"></div>
+        <img class="imgBase" src="${quizzesApi[i].image}">
+        <div class="chamada">
+            <h3>${quizzesApi[i].title}</h3>
+        </div>
+    </div>`
+    }
+}
