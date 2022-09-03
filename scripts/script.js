7160ef9ff56dd220fe33d25e0f7e55d104adb8c0
lujolfs <lucas.j.frank@gmail.com>
2022-09-03 02:51:33 -0300

    Implementação de mudança de página ao clicar em um dos quizzes; implementação de especificidade para a função playQuizz.

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 // Renderização dos quizzes
 const quizzesBox = document.querySelector('.api-quizz');
+const homePage = document.querySelector('main');
+const quizzPage = document.querySelector('.quizzPage');
 let quizzesApi = []; // Cria array vazio para preenchê-lo com as informações recebidas da API.
+let quizzesIndex = [];
 let quizzAtual; //Array onde a página de Quizz irá puxar as informações para usar no Quizz
 let qddPerguntas;
 renderQuizz(); // Chama a função render Quizz.
-
 
 function renderQuizz() {
     let quizzesInfo = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
@@ -20,9 +22,9 @@ function printQuizz(quizzesApi) {
     quizzesBox.innerHTML = '';
     console.log(quizzesApi);
 
-    for(let i = 0; i < 6; i++) { // Esse for é substituível por um map, mas não consegui aprender como
+    for(let i = 0; i < 6; i++) { // Esse for é substituível por um map, mas não consegui aprender como.
     quizzesBox.innerHTML = quizzesBox.innerHTML + `
-    <div class="quizz box">
+    <div id="index-${i}" class="quizz box" onclick="playQuizz(${i})">
         <div class="gradient"></div>
         <img class="imgBase" src="${quizzesApi[i].image}">
         <div class="chamada">
@@ -32,15 +34,18 @@ function printQuizz(quizzesApi) {
     }
 }
 
-function restartPage(){
+
+function restartPage() {
     window.location.reload();
 }
-function playQuizz(){
+
+function playQuizz(identity){
     const quizz = document.querySelector('.quizzPage');
-    quizzAtual = quizzesApi[0];
+    quizzAtual = quizzesApi[identity];
     console.log(quizzAtual.questions.length);
     console.log(quizzAtual);
-     quizz.innerHTML = ''
+    homePage.classList.add("hidden");
+    quizzPage.classList.remove("hidden");
 }
 
 function testeQuizz(){
