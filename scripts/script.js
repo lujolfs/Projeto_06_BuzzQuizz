dd2fe98edc95dc094e90c3f2b8f0a02bb096a630
davi-liporace <davimarquesr@gmail.com>
2022-09-04 20:10:01 -0300

    quizz faltando as respostas

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const quizzesBox = document.querySelector('.api-quizz');
 let quizzesApi = []; // Cria array vazio para preenchê-lo com as informações recebidas da API.
 let quizzAtual; //Array onde a página de Quizz irá puxar as informações para usar no Quizz
 let qddPerguntas;
+let i;
 renderQuizz(); // Chama a função render Quizz.
 
 
@@ -35,16 +36,72 @@ function printQuizz(quizzesApi) {
 function restartPage(){
     window.location.reload();
 }
+
+//Array que colocará as informações do quizz do servidor no html da página
+//dividir o Inner html em blocos para poder mexer neles separadamente
 function playQuizz(){
-    const quizz = document.querySelector('.quizzPage');
     quizzAtual = quizzesApi[0];
-    console.log(quizzAtual.questions.length);
-    console.log(quizzAtual);
-     quizz.innerHTML = ''
+    const quizz = document.querySelector('.quizzPage');
+    //bloco 1 - Header
+    const header = document.querySelector('.QuizzHeader');
+    header.innerHTML = `<img class="headerimg" src="${quizzAtual.image}" >  </img>
+     <h1>${quizzAtual.title}</h1>`;
+
+
+     //bloco das perguntas
+     const caixarespostas = document.querySelector('.respostas');
+     const caixaperguntas = document.querySelector('.PerguntasGerais');
+     caixaperguntas.innerHTML ='';
+         for (let i = 0; i <quizzAtual.questions.length; i++){
+     caixaperguntas.innerHTML = caixaperguntas.innerHTML +
+     `<div class="questionBox">
+     <div class="question1">
+         <h1>${quizzAtual.questions[i].title}</h1>
+     </div>
+     <div class="respostas"><div class="resposta1"><img class="imagemResposta"
+     src="${quizzAtual.questions[i].answers[0].image}">
+ </img>
+ <h1>${quizzAtual.questions[i].answers[0].text}</h1>
+</div>
+<div class="resposta1"><img class="imagemResposta"
+     src="${quizzAtual.questions[i].answers[1].image}">
+ </img>
+ <h1>${quizzAtual.questions[i].answers[1].text}</h1>
+</div><br>
+
+         
+     </div>
+
+ </div>`
+    
+}
+
+
+     //bloco do resultado (será oculto e mostrado apenas 1 após criação de função)   
+     const respostas = document.querySelector('.quizzResultBox');
+     respostas.innerHTML ='';
+     for (let i = 0; i <quizzAtual.levels.length; i++){
+        respostas.innerHTML = respostas.innerHTML +
+        `<div class="quizzResultBox ">
+            <div class="quizzResult">
+               <h1> X% de acertos: ${quizzAtual.levels[i].title}</h1>
+            </div>
+            <div class="img_text_box">
+                <img class="imgResult"
+                    src="${quizzAtual.levels[i].image}">
+                <h1> Resultado:${quizzAtual.levels[i].text} </h1>
+            </div>
+        </div>`  
+   }
+   // quizz.innerHTML = ''
+    console.log(quizzAtual.levels.length);
+    console.log(quizzAtual);     
 }
 
 function testeQuizz(){
     quizzAtual[0] = quizzesApi[0];
     console.log(quizzAtual[0]);
-    `<h1>${quizzAtual[0].title}</h1>`
+    //`<h1>${quizzAtual[0].title}</h1>
+   // header.innerHTML = header.innerHTML + `<img class="headerimg" src="${quizzAtual.image}" >  </img>
+    //<h1>${quizzAtual.title}</h1>`
 }
